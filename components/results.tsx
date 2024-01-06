1d3426e77ffcb49fecb4889052f9c3d60fc6a81e
John <johnorisanwo@gmail.com>
2024-01-06 17:59:54 +0100

    fixes

--- components/results.tsx
+++ components/results.tsx
@@ -1,19 +1,37 @@
 import { fetchByArea } from "@/app/fetch";
 import { fetchByMain } from "@/app/fetch";
 import { fetchByCategory } from "@/app/fetch";
+import { useEffect, useState } from "react";
 
 export function AreaResult({term}:{term:string}){
-    return (fetchByArea(term));
+    const [ results, setResults ] = useState<any>();
+
+    useEffect(() => {
+        const result = fetchByArea(term)
+        setResults(result)
+    },[ term ])
+
+    return results
 }
 
 export function  CategoryResult({term}:{term:string}){
-   return (
-    fetchByCategory(term)
-   )
+    const [ results, setResults ] = useState<any>();
+
+    useEffect(() => {
+        const result = fetchByCategory(term)
+        setResults(result)
+    },[ term ])
+
+    return results
 }
 
 export function IngredientResult({term}:{term:string}){
-    return (
-        fetchByMain(term)
-    )
+    const [ results, setResults ] = useState<any>();
+
+    useEffect(() => {
+        const result = fetchByMain(term)
+        setResults(result)
+    },[ term ])
+
+    return results
 }
