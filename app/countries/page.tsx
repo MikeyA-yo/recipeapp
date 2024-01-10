43088d1d469f1a3cf02fb0610d18eeac3640b9fc
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-01-10 15:18:09 +0100

    style added

--- app/countries/page.tsx
+++ app/countries/page.tsx
@@ -1,13 +1,17 @@
 import { Countries } from "@/components/results";
 import { ArrowLeftCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { CardsSkeleton } from "../ui/Skeletons";
+import { Suspense } from "react";
 
 
 export default function Page(){
     return (
         <div className="flex flex-col gap-5 items-center">
             <Link href={`/`} className="flex gap-2"><ArrowLeftCircleIcon className="h-6 w-6" aria-label="go back icon"/> Go back </Link>
-          <Countries />
+          <Suspense fallback = {<CardsSkeleton />}>
+              <Countries />
+           </Suspense>
         </div>
     )
 }
