620d857e1cd1ab067b675ad628625f5c913992e3
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-01-12 18:07:52 +0100

    well some empty div had to go

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,6 @@ export default function Home() {
       <Suspense fallback={<CardsSkeleton />} >
         <CardCate />
      </Suspense>
-     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-     </div>
     </main>
   );
 }
